bd5980a8408da4fc24f571c085c9158981efc9d9
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2025-04-22 22:34:52 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-let searchInput = document.querySelector(".searchInput");
+let searchInput = document.querySelector(".searchInput");   
+
+const url = "https://api.github.com/users/";
+
+const fetchProfile = async() => {
+
+    try {
+        const response = await fetch(`${url}SaleenaAhuja20`);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.log("error");
+    }
+}
