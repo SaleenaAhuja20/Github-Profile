fd5d3b085e9b6f5773e49cf4513fdafb0f8d1236
SaleenaAhuja20 <saleenaahuja038@gmail.com>
2025-04-22 22:48:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
-let searchInput = document.querySelector(".searchInput");   
+let searchInput = document.querySelector(".searchInput");  
+let searchButton = document.querySelector(".btn");
+let profileBox = document.querySelector(".profile-box");
 
-const url = "https://api.github.com/users/";
+const url = "https://api.github.com/users";
 
 const fetchProfile = async() => {
 
+    const userName = searchInput.value;
+
     try {
-        const response = await fetch(`${url}SaleenaAhuja20`);
+        const response = await fetch(`${url} / ${userName}`);
         const data = await response.json();
         console.log(data);
     } catch (error) {
-        console.log("error");
+        console.log({error});
     }
 }
+
+searchButton.addEventListener("click", () =>{
+    profileBox.remove(".hide");
+    fetchProfile();
+});
